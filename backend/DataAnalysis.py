a30c85d1f3650ae2663d6851a8ef49e71f471f4e
RedFusion Design <jan.wiechers04@gmail.com>
2020-09-05 18:17:43 +0200

    Some small change (now working)

--- backend/DataAnalysis.py
+++ backend/DataAnalysis.py
@@ -3,8 +3,9 @@ FrequencyList = [16.35,17.32,18.35,19.45,20.60,21.83,23.12,24.50,25.96,27.50,29.
                  261.6,277.2,293.7,311.1,329.6,349.2,370.0,392.0,415.3,440.0,466.2,493.9, 523.3,554.4,587.3,622.3,659.3,698.5,740.0,784.0,830.6,880.0,932.3,987.8,
                  1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,1865,1976, 2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951,
                  4186,4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902]
+ResultsTable = {}
 
-def Decode(Data):
+def Decode(Data,APIs):
     Outputs = []
     Inputs = []
     Normal = []
@@ -43,7 +44,6 @@ def Solve(Node,AllNodes):
     Dependend_Nodes = Dependencies[1],Dependencies[2]
     Data = []
     Type = Dependencies[0]
-    enum = 0
     for N in Dependend_Nodes:
         if(N in ResultsTable):
             Data.append(ResultsTable.get(N))
@@ -75,9 +75,8 @@ def RoundToNearest(Number):
             Target = i
     return Target
 
-ResultsTable = {}
+
 MyNetwork = "I1_1;I2_3;I3_8;N1_A,I1,I2;O1_I3,N1"
-APIs = [100,100,100,100,100,100,100,100,100,100]
 Sounds = Decode(MyNetwork)
 print(Sounds)
 
