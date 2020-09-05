1c8af63baa684b7f1792e2de226eb55566a94ff6
Your Name <hannah@dagmar-neschen.de>
2020-09-05 19:17:54 +0200

    mehrere Töne gleichzeitig + Pausen

--- get_sound_data.py
+++ get_sound_data.py
@@ -1,7 +1,7 @@
 s1 = {"wave":"sinus", "freq":"130", "length":"1"}
-s2 = {"wave":"sinus", "freq":"150", "length":"1"}
+s2 = {"wave":"sinus", "freq":"0", "length":"1"}
 s3 = {"wave":"sinus", "freq":"180", "length":"1"}
-s4 = {"wave":"sinus", "freq":"200", "length":"1"}
+s4 = {"wave":"sinus", "freq":"440", "length":"1"}
 
 data = [s1,s2,s3,s4]
 
