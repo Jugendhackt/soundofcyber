06b27e8f7236d729f2d63eac7f6e34297e19145b
Tristan Kaupa <tristan@kaupa.com>
2020-09-05 15:28:01 +0200

    Developing route

--- get_sound_data.py
+++ get_sound_data.py
@@ -1,4 +1,9 @@
-data = {"wave":"sinus", "freq":"133", "length":"4"}
+s1 = {"wave":"sinus", "freq":"133", "length":"4"}
+s2 = {"wave":"sinus", "freq":"90", "length":"4"}
+s2 = {"wave":"sinus", "freq":"65", "length":"4"}
+s2 = {"wave":"sinus", "freq":"458", "length":"8"}
+
+data = [s1,s2,s3,s4]
 
 def get_sound_data():
     return data
