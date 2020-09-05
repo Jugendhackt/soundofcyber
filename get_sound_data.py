cfc06d4e9db2d070b32babb29d5ca8529bf081e9
Tristan Kaupa <tristan@kaupa.com>
2020-09-05 15:28:43 +0200

    List of sound data

--- get_sound_data.py
+++ get_sound_data.py
@@ -1,7 +1,7 @@
 s1 = {"wave":"sinus", "freq":"133", "length":"4"}
 s2 = {"wave":"sinus", "freq":"90", "length":"4"}
-s2 = {"wave":"sinus", "freq":"65", "length":"4"}
-s2 = {"wave":"sinus", "freq":"458", "length":"8"}
+s3 = {"wave":"sinus", "freq":"65", "length":"4"}
+s4 = {"wave":"sinus", "freq":"458", "length":"8"}
 
 data = [s1,s2,s3,s4]
 
