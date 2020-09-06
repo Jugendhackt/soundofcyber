000054bc3e05fd1e67d822cbd5013b657cc83570
Tristan Kaupa <tristan@kaupa.com>
2020-09-06 10:17:34 +0200

    Directory Path

--- flask_server.py
+++ flask_server.py
@@ -8,10 +8,13 @@ import get_sound_data as GSD
 
 app = Flask(__name__)
 
+
+directory_path = pathlib.Path(__file__).parent.absolute()
+
 @app.route("/")
 def index():
     # Bekomme den Pfad der Index-HTML Seite: Ist im gleichen Ordner, also selber Pfad
-    html_path = pathlib.Path(__file__).parent.absolute() / "index.html"
+    html_path = directory_path / "index.html"
     website = open(html_path, "r").read()
     # Webseite als Index anzeigen
     return website
@@ -24,11 +27,11 @@ def get_sound_data():
 
 @app.route("/logo_dark_bg.svg")
 def logodark():
-    return open("logo_dark_bg.svg", encoding = "utf8").read(), 200, {'Content-Type': 'image/svg+xml charset=utf-8'}
+    return open(directory_path / "logo_dark_bg.svg", encoding = "utf8").read(), 200, {'Content-Type': 'image/svg+xml charset=utf-8'}
 
 @app.route("/logo.svg")
 def logo():
-    return open("logo.svg", encoding = "utf8").read(), 200, {'Content-Type': 'image/svg+xml charset=utf-8'}
+    return open(directory_path / "logo.svg", encoding = "utf8").read(), 200, {'Content-Type': 'image/svg+xml charset=utf-8'}
 
 def convert_to_json(data):
     return jsonify(data)
