2072326e4c1b79954120b1d7bca20ee26a820644
JH220 <admin@jh220.de>
2020-09-06 11:17:32 +0200

    Update request_data.py

--- backend/request_data.py
+++ backend/request_data.py
@@ -1,9 +1,11 @@
 import requests
-import os
+import time
 
 def get_data(apiNumber):
     if apiNumber == 0:
         response = requests.get('https://covid19-api.org/api/status')
+    elif apiNumber == 1:
+        response = requests.get('https://ridb.recreation.gov/api/v1/tours', params={'Authorization': 'Bearer b5677a68-ed4b-4781-befa-b04790fd5796'})
     else:
         response = {'status': 'invalid apiNumber'}
     return response
