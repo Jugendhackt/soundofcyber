dd872cc70e1f661fe3c9b7de14379ae81c54d331
RedFusion Design <jan.wiechers04@gmail.com>
2020-09-06 10:32:40 +0200

    Now using correct return System

--- backend/DataAnalysis.py
+++ backend/DataAnalysis.py
@@ -1,4 +1,6 @@
 import request_data as rq
+import time
+
 FrequencyList = [16.35,17.32,18.35,19.45,20.60,21.83,23.12,24.50,25.96,27.50,29.14,30.87, 32.70,34.65,36.71,38.89,41.20,43.65,46.25,49.00,51.91,55.00,58.27,61.74,
                  65.41,69.30,73.42,77.78,82.41,87.31,92.50,98.00,103.8,110.0,116.5,123.5, 130.8,138.6,146.8,155.6,164.8,174.6,185.0,196.0,207.7,220.0,233.1,246.9,
                  261.6,277.2,293.7,311.1,329.6,349.2,370.0,392.0,415.3,440.0,466.2,493.9, 523.3,554.4,587.3,622.3,659.3,698.5,740.0,784.0,830.6,880.0,932.3,987.8,
@@ -7,10 +9,16 @@ FrequencyList = [16.35,17.32,18.35,19.45,20.60,21.83,23.12,24.50,25.96,27.50,29.
 ResultsTable = {}
 
 def Interpret():
+    Return = []
     Data = str(rq.get_data(0))
     Data = int(Data.split(" ")[1].replace(">","").replace("[","").replace("]",""))
-    print(Data)
-    return Analysis().RoundToNearest(Data)
+    for i in range(0,10):
+        Current = time.time()
+        print(Data)
+        print(Current)
+        Note = {"freq" : Analysis().RoundToNearest(Data), "lenght": 0.2 + Current % 3}
+        Return.append(Note)
+    return Return
 
 class Analysis:
     def Decode(self,Data,APIs):
